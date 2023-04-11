5eaee4c519a9c4ce8e94b021476cb14afb89743e
Ella Byun <yantle@naver.com>
2023-04-11 21:29:16 +0900

    추가

---
+++ src/TailwindComponent.jsx
@@ -0,0 +1,8 @@
+export default function TailwindComponent() {
+  return (
+    <div>
+      <h1 className='text-2xl'>안녕!</h1>
+      <button className='bg-blue-500 rounded-xl px-2'>제출</button>
+    </div>
+  );
+}
